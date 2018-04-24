1431a809eb09cee64de297004be15391f986b992
Alex <onaga1958@yandex.ru>
2018-04-24 21:02:28 +0300

    basic signature

--- TLS/elliptic/elliptic_curve.py
+++ TLS/elliptic/elliptic_curve.py
@@ -1,3 +1,15 @@
 class EllipticCurve:
-    def __init__(self, id):
+    def __init__(self, curve_id):
+        pass
+
+    def summ(self, point_a, point_b):
+        pass
+
+    def double(self, point):
+        pass
+
+    def get_multiplicity(self, point):
+        pass
+
+    def is_on_curve(self, point):
         pass
