c4a4b7a090217685e37cefd8f7545e290f64b3a3
Alex <onaga1958@yandex.ru>
2018-05-07 23:55:57 +0300

    add multiply_by_number

--- TLS/elliptic/elliptic_curve.py
+++ TLS/elliptic/elliptic_curve.py
@@ -14,8 +14,20 @@ class EllipticCurve:
     def double(self, point):
         pass
 
-    def get_multiplicity(self, point):
-        pass
+    def multiply_by_number(self, point, number):
+        result = self.P
+        point_power = self.point
+        number = number % self.m
+
+        while True:
+            if number % 2 == 1:
+                result = self.summ(point_power, result)
+            if number < 2:
+                break
+            point_power = self.double(point_power)
+            number = number // 2
+
+        return result
 
     def is_on_curve(self, point):
         right_part = point.x ** 3 + self.a * point.x * point.z ** 2 + self.b * point.z ** 3
