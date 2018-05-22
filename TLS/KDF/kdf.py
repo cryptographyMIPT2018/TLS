2b8456637793a77bf92c3df86db8a05789c6d4b9
zakharovas <alexkru2011@gmail.com>
2018-05-23 00:45:02 +0300

    comments added

--- TLS/KDF/kdf.py
+++ TLS/KDF/kdf.py
@@ -2,5 +2,11 @@ from TLS.HMAC.hmac import *
 
 
 def KDF_TREE_256(K, label, seed, R, L, i):
+    """
+    :param K: input key
+    :param L: length_of_key_material
+    :param i: number_of_key
+    :return: K(i)
+    """
     return hmac256(K, i.to_bytes(R, byteorder="big") + label + bytes([0]) + seed +
                    L.to_bytes((L.bit_length() + 7) // 8, byteorder="big"))
