e09ca0b3d41303b3a1b46defb2a12c7280e04b02
Pavel <pavel.gramovich@diginetica.com>
2018-05-23 23:33:09 +0300

    decrypt added

--- TLS/kuznyechik/kuznyechik.pyx
+++ TLS/kuznyechik/kuznyechik.pyx
@@ -1,26 +1,35 @@
-ctypedef unsigned char byte
+from libcpp.pair cimport pair
 
-cdef extern from "src/Kuznechik.h":
-    void BuildTransformTable(byte keys[10][16], byte output[9][256][16])
-    void ExpandKey(byte* primary_key, byte keys[10][16])
-    void EncryptBlock(byte keys[10][16], byte XLS_transform_table[9][256][16], byte* block, byte* output)
+ctypedef unsigned char uchar
 
+cdef extern from "src/kusnyechik_impl.h":
+    void init()
+    
+    cdef cppclass Block:
+        Block(const uchar* ptr)
+
+    ctypedef pair[Block, Block] BlockPair
+
+    cdef cppclass Encryptor:
+        Encryptor(BlockPair key)
+        void encrypt(const uchar* src, uchar* dest)
+        void decrypt(const uchar* src, uchar* dest)
+
+init()
 
 cdef class CKuznyechik:
-    cdef byte key[32]
-    cdef byte keys[10][16]
-    cdef byte XLS_transform_table[9][256][16];
+    cdef Encryptor* enc
 
     def set_key(self, key: bytes):
-         self.key = key
-         ExpandKey(self.key, self.keys)
-         BuildTransformTable(self.keys, self.XLS_transform_table)
-        
+         self.enc = new Encryptor(BlockPair(Block(key[:16]), Block(key[16:])))
+
     def encrypt(self, plain_text: bytes) -> bytes:
         cipher_text = bytes([0 for _ in plain_text])
-        EncryptBlock(self.keys, self.XLS_transform_table, plain_text, cipher_text)
+        self.enc.encrypt(plain_text, cipher_text)
         return cipher_text
-    
+
     def decrypt(self, cipher_text: bytes) -> bytes:
-        return cipher_text
+        plain_text = bytes([0 for _ in cipher_text])
+        self.enc.decrypt(cipher_text, plain_text)
+        return plain_text
 
