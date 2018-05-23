9d61880fbb722d64b88c548544c318abc80356a0
Ibragimov Bulat <ibrbulat@yandex.ru>
2018-05-23 16:08:35 +0300

    writer finished

--- TLS/record/tls_network.py
+++ TLS/record/tls_network.py
@@ -1,4 +1,8 @@
 import socket
+from ..kuznyechik.mac import CMAC
+from ..kuznyechik.block_cipher import Kuznyechik
+from ..kuznyechik.block_cipher_mode import CTR_ACPKM
+from ..KDF.kdf import KDF_TREE_256
 
 HANDSHAKE_TYPE = 'Handshake'
 ALERT_TYPE = 'Alert'
@@ -8,8 +12,9 @@ APP_DATA_TYPE = 'ApplicationData'
 KEY_MAC_TYPE = 'key_mac'
 KEY_ENC_TYPE = 'key_enc'
 IV_TYPE = 'iv'
+SEED_TYPE = 'seed'
 
-KEY_TYPES = [KEY_MAC_TYPE, KEY_ENC_TYPE, IV_TYPE]
+KEY_TYPES = [KEY_MAC_TYPE, KEY_ENC_TYPE, IV_TYPE, SEED_TYPE]
 
 
 class TLSBasetext:
@@ -40,6 +45,7 @@ class Record:
         self._key_mac = None
         self._key_enc = None
         self._iv = None
+        self._seed = None
 
     def update_key(self, key_type, value):
         if key_type not in KEY_TYPES:
@@ -47,35 +53,49 @@ class Record:
         setattr(self, '_' + key_type, value)
 
     def enable_cipher_mode(self):
-        if self._key_mac is None or self._key_enc is None or self._iv is None:
-            raise AttributeError('keys have not been set')
+        for key_type in KEY_TYPES:
+            if getattr(self, '_' + key_type) is None:
+                raise AttributeError('{} has not been set'.format(key_type))
         self._is_cipher_mode = True
 
     def disable_cipher_mode(self):
+        for key_type in KEY_TYPES:
+            setattr(self, '_' + key_type, None)
         self._is_cipher_mode = False
 
     def _evaluate_mac(self, rec):
         seqnum_bytes = self._seqnum.to_bytes(8, byteorder='big')
         mac_data = seqnum_bytes + rec.to_bytes()
-        key_mac_seqnum = self._tlstree_mac()
-        #TODO MAC
-        return MAC(key_mac_seqnum, mac_ata)
+        key_mac_seqnum = self._tlstree(self._key_mac, self._seqnum)
+        block_cipher = Kuznyechik()
+        cmac = CMAC(block_cipher, len(mac_data))
+        return cmac.calculate(mac_data, key_mac_seqnum)
 
     def _encrypt(self, fragment, rec_mac):
         enc_data = fragment + rec_mac
-        key_enc_seqnum = _tlstree_enc()
+        key_enc_seqnum = self._tlstree(self._key_mac, self._seqnum)
         iv_seqnum = (int.from_bytes(self._iv) + self._seqnum) % (2 ** 64)
         iv_seqnum = iv_seqnum.to_bytes(8, byteorder='big')
-        #TODO ENC
-        return ENC(key_enc_seqnum, iv_seqnum, enc_data)
+        block_cipher = Kuznyechik()
+        enc = CTR_ACPKM(block_cipher, 32, 16)
+        return enc.encrypt(enc_data, key_enc_seqnum, iv_seqnum)
 
-    def _tlstree_mac(self):
-        #TODO
-        pass
+    def _diver(self, level, K, D):
+        R = 1
+        L = 256
+        return KDF_TREE_256(K, str.encode('level' + str(level)), self._seed, R, L, self._seqnum + 1)
 
-    def _tlstree_enc(self):
-        #TODO
-        pass
+    def _tlstree(self, key, i):
+        C1 = int.from_bytes(b'\xff\xff\xff\xff\x00\x00\x00\x00')
+        C2 = int.from_bytes(b'\xff\xff\xff\xff\xff\xf8\x00\x00')
+        C3 = int.from_bytes(b'\xff\xff\xff\xff\xff\xff\xff\xc0')
+        return self._diver(3,
+            self._diver(
+                2, self._diver(1, key, (i & C1).to_bytes(8, byteorder='big')),
+                (i & C2).to_bytes(8, byteorder='big')
+            ),
+            (i & C3).to_bytes(8, byteorder='big')
+        )
 
 
 class Writer(Record):
@@ -124,7 +144,17 @@ class Writer(Record):
 
 
 class Reader(Record):
-    pass
+    def parse_header(self, header):
+        byte2type = {b'\x14': CCS_TYPE,
+                     b'\x15': ALERT_TYPE,
+                     b'\x16': HANDSHAKE_TYPE,
+                     b'\x17': APP_DATA_TYPE}
+        msg_type = byte2type[header[0]]
+        length = int.from_bytes(header[3:], byteorder='big')
+        return msg_type, length
+
+    def parse_fragment(self, fragment):
+        self._seqnum += 1
 
 
 
@@ -133,7 +163,7 @@ class TLSNetwork:
         self.host = host
         self.port = port
         self._writer = Writer()
-        self._reader = Sender()
+        self._reader = Reader()
         self._socket = socket.socket()
 
     def __enter__(self):
@@ -169,4 +199,3 @@ class TLSNetworkClient(TLSNetwork):
 
     def connect(self):
         self.connect((self.host, self.port))
-        
