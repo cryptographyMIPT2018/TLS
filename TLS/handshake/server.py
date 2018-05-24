57c8c799cc09c718c0a44e94738803607d4aa262
Alex Zabelin <onaga1958@yandex.ru>
2018-05-24 11:02:18 +0300

    add change_cipher_spec

--- TLS/handshake/server.py
+++ TLS/handshake/server.py
@@ -5,6 +5,7 @@ handshake_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(handshake_dir, '../../'))
 
 from TLS.record.tls_network import HANDSHAKE_TYPE, CCS_TYPE
+from TLS.record.tls_network import KEY_MAC_TYPE, KEY_ENC_TYPE, IV_TYPE
 from TLS.elliptic.elliptic_curve import Point
 from TLS.certificate.public_keys import public_keys
 import time
@@ -14,6 +15,7 @@ from message_structures import CERTIFICATE_MESSAGE, CLIENT_KEY_EXCHANGE_MESSAGE
 from message_structures import CERTIFICATE_REQUEST_MESSAGE, SERVER_HELLO_DONE_MESSAGE
 from message_structures import CERTIFICATE_VERIFY_MESSAGE, FINISHED_MESSAGE
 from message_structures import CHANGE_CIPHER_SPEC
+
 
 class HandshakeServer:
     def __init__(self, network, user_id):
@@ -33,8 +35,9 @@ class HandshakeServer:
 
     def _send(self, message_data, message_structures=None, record_msg_type=HANDSHAKE_TYPE):
         if message_structures is None:
-            message_
             byte_message = message_structures.to_bytes(message_data)
+        else:
+            byte_message = message_data
         self._network.send(record_msg_type, byte_message)
 
     def handshake(self):
@@ -95,6 +98,24 @@ class HandshakeServer:
     def _send_hello_done(self):
         self._send(b'', SERVER_HELLO_DONE_MESSAGE)
 
-    def _send_change_cipher_spec():
-        self._change_cipher_spec()
+    def _send_change_cipher_spec(self):
+        self._change_cipher_spec('write')
         self._send(CHANGE_CIPHER_SPEC, record_msg_type=CCS_TYPE)
+
+    def _change_cipher_spec(self, record_type):
+        """
+        Args:
+            record_type: str
+                'reader' or 'writer'
+        """
+        allowed = ['reader', 'writer']
+        assert record_type in allowed, 'record_type should be in {}, got {}'.format(allowed, record_type)
+        if record_type == 'reader':
+            key_type = 'read'
+        else:
+            key_type = 'write'
+
+        record = getattr(self._network, record_type)
+        record.update_key(KEY_MAC_TYPE, getattr(self, '_k_' + key_type + '_mac_s'))
+        record.update_key(KEY_ENC_TYPE, getattr(self, '_k_' + key_type + '_enc_s'))
+        record.update_key(IV_TYPE, getattr(self, '_iv_' + key_type + '_s'))
